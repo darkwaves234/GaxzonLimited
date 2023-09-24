e9ee99bc5ff0687648b66d0552430d28ec0a313d
Hirun2007 <145740879+Hirun2007@users.noreply.github.com>
2023-09-25 03:37:58 +0530

    up

---
+++ gaxzonwork1/searchbox.js
@@ -0,0 +1,4 @@
+document.getElementById("search-button").addEventListener("click", function () {
+  var searchTerm = document.getElementById("search-input").value;
+  alert("You searched for: " + searchTerm);
+});
